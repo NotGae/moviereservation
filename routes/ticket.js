6ef5d2faad87242ff425ebdb2abe5539778d4716
notgae <yla262652@gmail.com>
2024-11-20 13:53:21 +0900

    booking insert문 완료

--- routes/ticket.js
+++ routes/ticket.js
@@ -30,8 +30,45 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/booking', async (req, res) => {
-  console.log(req.body);
-  //여기서 이제 거의 다 된듯
+  const seatsArr = JSON.parse(req.body.seats);
+  const usrPhoneNum = req.body.phoneNumber;
+  const usrPassword = req.body.pwd;
+
+  console.log(usrPhoneNum);
+  console.log(usrPassword);
+  // 코드 이렇게 바꾸기. sql 인젝션땜에
+  const [row] = await pool.query(
+    'SELECT userId from users WHERE phoneNumber = ? and password = ?;',
+    [usrPhoneNum, usrPassword]
+  );
+  let userId = row;
+  if (userId.length === 0) {
+    await pool.query('INSERT INTO users(phoneNumber, password) values(?, ?);', [
+      usrPhoneNum,
+      usrPassword,
+    ]);
+    const [row2] = await pool.query(
+      'SELECT userId from users WHERE phoneNumber = ? and password = ?;',
+      [usrPhoneNum, usrPassword]
+    );
+    // 이제 id있음.
+    userId = row2[0];
+  }
+  console.log(userId);
+  for (let i = 0; i < seatsArr.length; i++) {
+    // 넣기 전에 먼저 users테이블에서 찾기. 없으면 users테이블에 삽입 후 해당 id 가져오기.
+    // bookingId는 그냥 auto incre로 처리
+    await pool.query(
+      'INSERT INTO bookings(bookingDate, bookingTime, screeningMovieId, userId, seatId, hallId, theaterId) values(CURDATE(), CURTIME(), ?, ?, ?, ?, ?);',
+      [
+        seatsArr[i].screeningMovieId,
+        userId.userId,
+        seatsArr[i].seatId,
+        seatsArr[i].hallId,
+        seatsArr[i].theaterId,
+      ]
+    );
+  }
   res.render('completeBooking.ejs');
 });
 
